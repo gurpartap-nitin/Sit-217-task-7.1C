4883ab62c7b62cbd55501427c6772e0cafcc9aec
gurpartap-nitin <83549676+gurpartap-nitin@users.noreply.github.com>
2021-05-11 01:58:29 +0530

    Add files via upload

---
+++ project.ino
@@ -0,0 +1,90 @@
+#define in1 3 //Define motor driver pins 
+#define in2 5
+#define in3 6
+#define in4 9
+int command; //variable to store app command state.
+int Speed = 255; // speed of motors which can be from 1-255
+int Speedsec;
+void setup() {
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  Serial.begin(9600);  //Set the baud rate to your Bluetooth module.
+}
+void loop() {
+  if (Serial.available() > 0) {
+    command = Serial.read();
+    Stop(); //Initialize with motors stoped.
+    switch (command) {
+      case 'F':
+        forward();
+        break;
+      case 'B':
+        back();
+        break;
+      case 'L':
+        left();
+        break;
+      case 'R':
+        right();
+        break;
+      case 'G':
+        forwardleft();
+        break;
+      case 'I':
+        forwardright();
+        break;
+      case 'H':
+        backleft();
+        break;
+      case 'J':
+        backright();
+        break;
+    }
+    
+  }
+}
+
+void forward() {
+  analogWrite(in1, Speed);
+  analogWrite(in3, Speed);
+}
+
+void back() {
+  analogWrite(in2, Speed);
+  analogWrite(in4, Speed);
+}
+
+void left() {
+  analogWrite(in3, Speed);
+  analogWrite(in2, Speed);
+}
+
+void right() {
+  analogWrite(in4, Speed);
+  analogWrite(in1, Speed);
+}
+void forwardleft() {
+  analogWrite(in1, Speedsec);
+  analogWrite(in3, Speed);
+}
+void forwardright() {
+  analogWrite(in1, Speed);
+  analogWrite(in3, Speedsec);
+}
+void backright() {
+  analogWrite(in2, Speed);
+  analogWrite(in4, Speedsec);
+}
+void backleft() {
+  analogWrite(in2, Speedsec);
+  analogWrite(in4, Speed);
+}
+
+void Stop() {
+  analogWrite(in1, 0);
+  analogWrite(in2, 0);
+  analogWrite(in3, 0);
+  analogWrite(in4, 0);
+}
